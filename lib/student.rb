98be746d4a700fee4a3b1cda710189ebe2ecb4b7
mklavuhn <michaelklavuhn3@gmail.com>
2021-08-23 09:41:45 -0700

    commit tree 45a15e4d670a054d63812807a52d216633d16a63

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
+
+    
+
+    def initialize
+        @knowledge = [] 
+    end
+
+    def learn(phrase)
+        @knowledge << phrase
+    end
+
+    def knowledge
+        @knowledge
+    end
+    
+
+
 
 end
